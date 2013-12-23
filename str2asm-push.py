b2240bb35b824e7e3286d0de2d0113c77fa8af5c
Boogy <theboogymaster@gmail.com>
2013-12-23 14:01:35 +0100

    return a string that can be used in asm programs

---
+++ str2asm-push.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# Output a string that can be used directly in asembly
+#
+import sys
+
+input = sys.argv[1]
+
+# Reverse the string
+rev_input = input[::-1]
+
+# Encode it in hex
+rev_input_hex = rev_input.encode('hex')
+
+print "\n[+] Hex string : \n" + rev_input_hex
+print "\n[+] asm output:"
+for item in map( "".join, zip(*[iter(rev_input_hex)]*8)):
+  print "push 0x" + item
+
+print
