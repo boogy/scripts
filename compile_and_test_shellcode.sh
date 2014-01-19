3924bd3e0e5df9593d97c90442eff38d0e8dc22b
Boogy <theboogymaster>
2014-01-19 22:25:18 +0100

    changed to 7 columns the cut

--- compile_and_test_shellcode.sh
+++ compile_and_test_shellcode.sh
@@ -19,7 +19,7 @@ function compile()
     ld -m elf_i386 $FILE_NAME.o -o $FILE_NAME
   fi
   
-  SHELLCODE=$(objdump -d $FILE_NAME.o|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-6 -d' '|tr -s ' '| \
+  SHELLCODE=$(objdump -d $FILE_NAME.o|grep '[0-9a-f]:'|grep -v 'file'|cut -f2 -d:|cut -f1-7 -d' '|tr -s ' '| \
   tr '\t' ' '|sed 's/ $//g'|sed 's/ /\\x/g'|paste -d '' -s |sed 's/^/"/'|sed 's/$/"/g')
   
   echo
