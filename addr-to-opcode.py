d289d65f8fb969abeda106a7f84e051a7bde1bca
boogy <theboogymaster@gmail.com>
2014-04-25 08:09:51 +0200

    reverse address

---
+++ addr-to-opcode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import sys
+
+try:
+  input = sys.argv[1].replace('0x', '')
+except IndexError:
+  print "[-] You must supply a string to the script ...!"
+  sys.exit(1)
+
+print "String lenght " + str(len(input))
+
+s = [ input[i:i+2] for i in range(0, len(input), 2) ]
+
+for item in s[::-1]:
+	sys.stdout.write("\\x" + item)
+print
