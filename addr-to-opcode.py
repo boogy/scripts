b20eb24fd4f92755434e138d49e12f8dca0a965f
boogy <theboogymaster@gmail.com>
2014-04-25 13:53:17 +0200

    reverse address

--- addr-to-opcode.py
+++ addr-to-opcode.py
@@ -9,10 +9,9 @@ except IndexError:
   print "[-] You must supply a string to the script ...!"
   sys.exit(1)
 
-print "String lenght " + str(len(input))
+#print "String lenght " + str(len(input))
 
 s = [ input[i:i+2] for i in range(0, len(input), 2) ]
 
 for item in s[::-1]:
 	sys.stdout.write("\\x" + item)
-print
