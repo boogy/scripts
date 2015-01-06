a6867f7c0ab364c19d983f0138d0e1787b63d471
Boogy <boogy@users.noreply.github.com>
2015-01-06 13:03:10 +0100

    run_check_return
    paramiko command execution with command return code

--- mypylib.py
+++ mypylib.py
@@ -5,3 +5,11 @@ def chunks(l, n):
     """ Yield successive n-sized chunks from l"""
     for i in xrange(0, len(l), n):
         yield l[i:i+n]
+
+def run_check_return(host, cmd):
+    import paramiko
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(host)
+    stdin, stdout, stderr = client.exec_command(cmd)
+    return stdout.channel.recv_exit_status()    # status is 0 if OK
