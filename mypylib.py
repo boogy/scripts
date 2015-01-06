a910ca7a6a7c838b22b3cc97fe6ad452579ef4dd
Boogy <boogy@users.noreply.github.com>
2015-01-06 16:46:08 +0100

    threaded function added
    Function decorator to thread the decorated function

--- mypylib.py
+++ mypylib.py
@@ -13,3 +13,27 @@ def run_check_return(host, cmd):
     client.connect(host)
     stdin, stdout, stderr = client.exec_command(cmd)
     return stdout.channel.recv_exit_status()    # status is 0 if OK
+
+def threaded(f, daemon=True):
+    """Function decorator to use threading"""
+    import threading
+    import Queue
+    def wrapped_f(q, *args, **kwargs):
+        """this function calls the decorated function and puts the
+        result in a queue"""
+        ret = f(*args, **kwargs)
+        q.put(ret)
+
+    def wrap(*args, **kwargs):
+        """this is the function returned from the decorator. It fires off
+        wrapped_f in a new thread and returns the thread object with
+        the result queue attached"""
+        q = Queue.Queue()
+        t = threading.Thread(target=wrapped_f, args=(q,)+args, kwargs=kwargs)
+        t.setDaemon(daemon)
+        t.setName(args[2])
+        print "[+] Starting thread name %s daemon %s" % (t.name, t.isDaemon())
+        t.start()
+        t.result_queue = q
+        return t
+    return wrap
