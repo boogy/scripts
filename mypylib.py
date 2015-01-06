cc297d9546c38960357f09c06f936c5c3e5cba65
Boogy <boogy@users.noreply.github.com>
2015-01-06 16:49:13 +0100

    added description

--- mypylib.py
+++ mypylib.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 #
+# A python file with useful functions to be able
+# to find them fast when needed
+#
 def chunks(l, n):
     """ Yield successive n-sized chunks from l"""
     for i in xrange(0, len(l), n):
